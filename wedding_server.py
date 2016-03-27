3d2ca6fa53ecdaf9a96724ced62a84772ec6568b
wlgmy <991422465@qq.com>
2016-03-27 21:55:53 +0800

    change server.py add insert and update info

--- wedding_server.py
+++ wedding_server.py
@@ -48,7 +48,43 @@ def shutdown():
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         greeting = self.get_argument('greeting', 'Hello')
-        self.write(greeting + ', tornado!')
+        name = self.get_argument('name','')
+        tel = self.get_argument('tel','')
+        person = self.get_argument('person','')
+        context = self.get_argument('context','')
+        place = {}
+
+        place['SH'] = self.get_argument('sh','false') == 'true'
+        place['MY'] = self.get_argument('my','false') == 'true'
+        place['TC'] = self.get_argument('tc','false') == 'true'
+        try:
+            placelist = list(place)
+        except Exception,e:
+            placelist = []
+        logging.info('name:'+name)
+        logging.info('tel:'+tel)
+        logging.info('person:'+person)
+        logging.info('context:'+context)
+        ret = {}
+        ret['code'] = 0
+        if(name == ''):
+            ret['code'] = 1
+            ret['message'] = 'plese input your name'
+        if(ret['code'] == 0):
+            logging.info('success!')
+        ret['place'] = place
+        if(ret['code'] == 0):
+            num = db['wedding_test'].find({'name':name,'tel':tel}).count()
+            logging.info('name = '+str(name)+'has '+str(num)+' records')
+            if(num == 0):
+                log = db['wedding_test'].insert({'name':name,'tel':tel,'person':person,'context':context,'place':place})
+            else:
+                log = db['wedding_test'].update({'name':name,'tel':tel},{'person':person,'context':context,'place':place})
+            logging.info(log)
+        retStr = json.dumps(ret,ensure_ascii=False)
+        self.write(retStr)
+
+
 
 def wedding(port, start_ioloop=True):
     """
